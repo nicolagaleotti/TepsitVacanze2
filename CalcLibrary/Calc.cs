98711240ad794a56e4fb4e95932de535479696a6
Nicola Galeotti <nicola.galeotti9@gmail.com>
2020-01-04 20:24:07 +0100

    ricopiato

---
+++ CalcLibrary/Calc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcLibrary
+{
+    class Calc
+    {
+        public static double RadQ (double num)
+        {
+            double resp = 0;
+            if (num >= 0)
+                resp = Math.Sqrt(num);
+            else
+                resp = double.NaN;
+            return resp;
+        }
+    }
+}
